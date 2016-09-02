2badc8779e26187a09157bce72937bd09e055bef
Jose Angel <homatre@gmail.com>
2016-09-02 02:33:36 -0500

    Adding multiple prints

--- hello.c
+++ hello.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+void print_n_times(unsigned char in);
+
 void main(void){
-    printf("Hello World\n");
+    print_n_times(8);
+}
+
+
+void print_n_times(unsigned char in){
+    unsigned char i=1;
+    while(in){
+        printf("%d: Hello World\n",i);
+        i++;
+        in--;
+    }
 }
